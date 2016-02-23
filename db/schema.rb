aa79421180d50e1ed64880c4de94539cbe645c30
vismarach <christian.vismara@nttdata.com>
2016-02-23 11:47:55 +0100

    section 'handeling permissions' completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117160922) do
+ActiveRecord::Schema.define(version: 20160223072533) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "desciption"
+    t.float    "price"
+    t.boolean  "availability"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
